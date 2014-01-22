7d1d292ecb00f4f66afb926288d7c40a590a8287
Reto Aebersold <aeby@atizo.com>
2014-01-22 11:40:41 +0100

    add core dependencies

--- braindump/settings.py
+++ braindump/settings.py
@@ -36,6 +36,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',    
+    'rest_framework',
+    'storages',
+    'south',
 )
 
 MIDDLEWARE_CLASSES = (
