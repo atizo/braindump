183247744c8745f0d8a53567784d71f9415e38da
Reto Aebersold <aeby@atizo.com>
2014-01-23 11:55:10 +0100

    add angular client

--- braindump/settings.py
+++ braindump/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '%g#i3#6@*6v%81z)xsx6a*4zb3k^%3!b5*aof38^w7f8c6l6=q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = not bool(os.environ.get('DJANGO_NO_DEBUG', ''))
 
 TEMPLATE_DEBUG = True
 
@@ -36,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions',    
+    'django_extensions',
     'rest_framework',
     'storages',
     'south',
@@ -61,6 +62,7 @@ WSGI_APPLICATION = 'braindump.wsgi.application'
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
+
 DATABASES = {
     'default': dj_database_url.config(default='postgres://braindump:test@localhost/braindump')
 }
@@ -78,20 +80,29 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static asset configuration
-import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+if DEBUG:
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, '../client/app/static'),
+    )
+
+    TEMPLATE_DIRS = (
+        os.path.join(BASE_DIR, '../client/app'),
+    )
+else:
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
+
+    TEMPLATE_DIRS = (
+        os.path.join(BASE_DIR, 'templates'),
+    )
---
+++ client/app/static/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('clientApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'static/views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
