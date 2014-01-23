6d45e82280aea62366236ab3dcb9ff0936d7797e
Reto Aebersold <aeby@atizo.com>
2014-01-23 13:48:18 +0100

    add favicons

--- braindump/urls.py
+++ braindump/urls.py
@@ -1,16 +1,25 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
+import os
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       url(r'^admin/', include(admin.site.urls)),
-                       url(r'', include(include('api.urls'))),
                        url(r'^$', TemplateView.as_view(template_name="index.html"), name='home'),
+
+                       # REST API
+                       url(r'', include(include('api.urls'))),
+
+                       # admin interface
+                       url(r'^admin/', include(admin.site.urls)),
+
+                       # root files
+                       url(r'^(?P<path>favicon\.ico|apple-touch-icon\.png)$',
+                           'django.views.static.serve',
+                           kwargs={'document_root': os.path.join(settings.BASE_DIR, 'root')}),
 )
 
 if settings.DEBUG:
