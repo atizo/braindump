790cea0782fb8b3785cd11be5404e1e11efd8e3d
Reto Aebersold <aeby@atizo.com>
2014-01-24 16:10:38 +0100

    braingstorming form update

--- client/app/static/scripts/controllers/start.js
+++ client/app/static/scripts/controllers/start.js
@@ -3,9 +3,16 @@
 angular.module('braind')
   .controller('StartCtrl', ['$scope', 'Restangular', function ($scope, Restangular) {
     var brainstormings = Restangular.all('api/brainstormings/');
-    $scope.bs = {};
+
+    $scope.reset = function () {
+      $scope.bs = {
+        question: '',
+        creatorEmail: '',
+        details: ''
+      };
+    };
     $scope.create = function () {
-      console.info($scope.bs);
       brainstormings.post($scope.bs);
     };
+    $scope.reset();
   }]);
