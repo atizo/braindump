e20bd4b287b6fbcca1a5b7acd00b7d317429361a
Reto Aebersold <aeby@atizo.com>
2014-01-27 09:44:32 +0100

    fix tests

---
+++ client/test/spec/controllers/start.js
@@ -0,0 +1,27 @@
+'use strict';
+
+describe('Controller: MainCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('braind'));
+
+  var MainCtrl,
+    scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    MainCtrl = $controller('StartCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('should attach an empty brainstorming to the scope', function () {
+    var bs = {
+      question: '',
+      creatorEmail: '',
+      details: ''
+    };
+    expect(scope.bs).toEqual(bs);
+  });
+});
