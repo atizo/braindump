7d7244d829b45dcf16b2041432152fd3d024c11c
Mathis Hofer <hupf@atizo.com>
2014-01-28 10:51:54 +0100

    simplify brainstorming url pattern

--- brainstorming/urls.py
+++ brainstorming/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 admin.autodiscover()
 
 urlpatterns = patterns('brainstorming.views',
-                       url(r'^$', 'index', name='home'),
-                       url(r'^(?P<brainstorming_slug>\w{12})(/.*)*$', 'brainstorming', name='brainstorming'),
-                       url(r'^.*$', TemplateView.as_view(template_name="index.html")),
-)
+    url(r'^$', 'index', name='home'),
+    url(r'^(?P<brainstorming_slug>\w{12})/?', 'brainstorming', name='brainstorming'),
+    url(r'^.*$', TemplateView.as_view(template_name="index.html")),
+) 
