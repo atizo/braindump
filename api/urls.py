882671977d9406f5606f6a050a53dc8e1dd65194
Mathis Hofer <hupf@atizo.com>
2014-01-28 10:30:43 +0100

    use drf-nested-routers to nest brainstorming/idea resources

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,17 @@
 from brainstorming.viewsets import BrainstormingViewSet, IdeaViewSet
 from django.conf.urls import patterns, url, include
-from rest_framework import routers
+from rest_framework_nested.routers import NestedSimpleRouter, SimpleRouter
 
 
-router = routers.DefaultRouter()
+router = SimpleRouter()
 router.register(r'brainstormings', BrainstormingViewSet)
-router.register(r'ideas', IdeaViewSet)
 
+brainstormings_router = NestedSimpleRouter(router,
+    r'brainstormings', lookup='brainstorming')
+brainstormings_router.register(r'ideas', IdeaViewSet)
 
 urlpatterns = patterns('',
     url(r'^api/', include(router.urls)),
+    url(r'^api/', include(brainstormings_router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
