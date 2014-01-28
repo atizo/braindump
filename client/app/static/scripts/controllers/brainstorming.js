5f553c93d1281f09770f28784d17e437add7c9f7
Mathis Hofer <hupf@atizo.com>
2014-01-28 15:01:37 +0100

    client: add brainstorming form

--- client/app/static/scripts/controllers/brainstorming.js
+++ client/app/static/scripts/controllers/brainstorming.js
@@ -2,6 +2,19 @@
 
 angular.module('braind')
   .controller('BrainstormingCtrl', ['$scope', 'brainstormingService', function ($scope, brainstormingService) {
-    console.info(brainstormingService.getBrainstorming());
-    $scope.bs = {};
+    $scope.brainstorming = brainstormingService.getBrainstorming();
+    $scope.ideas = brainstormingService.getIdeas();
+
+    $scope.create = function () {
+
+    };
+
+    $scope.reset = function () {
+      $scope.formData = {
+        title: '',
+        text: ''
+      };
+    };
+
+    $scope.reset();
   }]);
