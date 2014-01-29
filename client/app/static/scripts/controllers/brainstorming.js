60576149c3989cc1c817f45385ee725c7afa40ca
Mathis Hofer <hupf@atizo.com>
2014-01-29 14:21:07 +0100

    client: basic ideas layout and styling; add Roboto 100 font

--- client/app/static/scripts/controllers/brainstorming.js
+++ client/app/static/scripts/controllers/brainstorming.js
@@ -11,6 +11,23 @@ angular.module('braind')
         $scope.ideas = obj;
       });
 
+      $scope.getIdeaSizeClassName = function (idea) {
+        var l = idea.title.length + idea.text.length;
+        if (l <= 25) {
+          return 'length-25';
+        }
+        if (l <= 50) {
+          return 'length-50';
+        }
+        if (l <= 150) {
+          return 'length-150';
+        }
+        if (l <= 300) {
+          return 'length-300';
+        }
+        return '';
+      }
+
       $scope.create = function () {
         brainstormingService.createIdea($scope.formData).then(function () {
           $scope.user.name = $scope.formData.creatorName;
