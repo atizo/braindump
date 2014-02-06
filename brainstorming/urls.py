515db0e5d075184d6ae95f739134db21d30e7851
Reto Aebersold <aeby@atizo.com>
2014-02-06 15:51:37 +0100

    fix url order

--- brainstorming/urls.py
+++ brainstorming/urls.py
@@ -6,8 +6,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('brainstorming.views',
                        url(r'^$', 'index', name='home'),
-                       url(r'^(?P<brainstorming_id>\w{12})/?', 'brainstorming', name='brainstorming'),
                        url(r'^(?P<brainstorming_id>\w{12})/notification$', 'notification', name='notification'),
                        url(r'^(?P<brainstorming_id>\w{12})/edit$', 'edit', name='edit'),
+                       url(r'^(?P<brainstorming_id>\w{12})/?', 'brainstorming', name='brainstorming'),
                        url(r'^.*$', TemplateView.as_view(template_name="index.html")),
 ) 
