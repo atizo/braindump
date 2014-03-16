cd494f23d3ce398f8f3c999b47e2e44084077c84
Reto Aebersold <aeby@atizo.com>
2014-03-16 12:17:48 -0600

    simple modal dialog

---
+++ client/app/static/scripts/directives/modal.js
@@ -0,0 +1,55 @@
+(function () {
+  'use strict';
+
+  angular.module('braind')
+    .directive('bdModal', ['$window', '$rootScope', function ($window, $rootScope) {
+      return {
+        restrict: 'C',
+        scope: {},
+        link: {
+          pre: function (scope, element) {
+
+            var closeModal = function () {
+              element.css('display', 'none');
+              angular.element('.bd-backdrop').css('display', 'none');
+              angular.element('body').removeClass('modal-open');
+            };
+
+            var close = function (evt) {
+              if (evt.target === evt.currentTarget) {
+                evt.preventDefault();
+                evt.stopPropagation();
+                closeModal();
+              }
+            };
+
+            element.bind('click', close);
+
+            var setMaxHeight = function () {
+              var maxContainer = element.find('.height-limit');
+              var vh = angular.element($window).height();
+              maxContainer.css('max-height', (vh - vh * 0.02 - 100) + 'px');
+            };
+
+            $rootScope.$on('bd:mopen', function (event, user) {
+              setMaxHeight();
+              element.css('display', 'block');
+              angular.element('.bd-backdrop').css('display', 'block');
+              angular.element('body').addClass('modal-open');
+            });
+
+            $rootScope.$on('bd:mclose', function (event, user) {
+              closeModal();
+            });
+
+            angular.element($window).bind('resize', setMaxHeight);
+
+            element.on('$destroy', function () {
+              angular.element($window).unbind('resize', setMaxHeight);
+              element.unbind('click', close);
+            });
+          }
+        }
+      };
+    }]);
+}());
