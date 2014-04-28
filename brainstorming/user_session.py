d8b4c05ebf16e1ecf274fc523c78ab2000affcfe
Reto Aebersold <aeby@atizo.com>
2014-04-28 11:18:23 -0600

    fix empty brainstromign history

--- brainstorming/user_session.py
+++ brainstorming/user_session.py
@@ -9,7 +9,7 @@ BS_HISTORY_KEY = 'BS_HISTORY'
 
 def update_bs_history(session, bid):
     if not bid or bid == settings.DEMO_PROJECT:
-        return session[BS_HISTORY_KEY]
+        return session.get(BS_HISTORY_KEY, [])
 
     history = collections.deque(session.get(BS_HISTORY_KEY, []), maxlen=10)
 
